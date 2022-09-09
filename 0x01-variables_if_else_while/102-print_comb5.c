9f8aafcaabcf65117c522fcd3f7c55c2cee04644
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-09 15:31:23 -0700

    102-print_comb5.c m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,43 +7,43 @@
  */
 int main(void)
 {
-	int i, e, g, h, op1, op2;
+int i, e, g, h, op1, op2;
 
-	i = e = g = h = 48;
-	while (h < 58)
+i = e = g = h = 48;
+while (h < 58)
+{
+        g = 48;
+	while (g < 58)
 	{
-		g = 48;
-		while (g < 58)
+		e = 48;
+		while (e < 58)
 		{
-			e = 48;
-			while (e < 58)
+			i = 48;
+			while (i < 58)
 			{
-				i = 48;
-				while (i < 58)
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
 				{
-					op1 = (h * 10) + g;
-					op2 = (h * 10) + i;
-					if (op1 < op2)
-					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					i++;
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
-				e++;
+				i++;
 			}
-			g++;
+			e++;
 		}
-		h++;
+		g++;
 	}
-	putchar('\n');
-	return (0);
+	h++;
+}
+putchar('\n');
+return (0);
 }
 
