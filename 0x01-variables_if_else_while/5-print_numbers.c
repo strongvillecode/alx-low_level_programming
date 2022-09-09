ad79726ccd4c93e80ed4571042752fcdb5a16457
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-09 13:31:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9.
+ *
+ * Return: Always ) (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
