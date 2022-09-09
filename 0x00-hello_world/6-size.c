b6e09a9c1233765f3c38052ca07ae48d87cbef2e
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-08 18:16:10 -0700

    Write a C program that prints the size of various types on the computer it is compiled and run on.
    You should produce the exact same output as in the example

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main -is the entry point for the program.
+ *main-print size of characters.
  *
- *Return:zero if no error,Non-zero value if errors.
+ *Return:Always 0 (success)
  */
 int main(void)
 {
