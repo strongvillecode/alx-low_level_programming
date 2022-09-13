300d206cfd52c1a45fc41b6b227ac8a0f7a0df26
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-13 12:45:41 -0700

    task 02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -18,9 +18,9 @@ void print_alphabet_x10(void)
 		while (ch <= 'z')
 		{
 			_putchar(ch);
-																				ch++;
+			ch++;
 		}
 		alphabet_count++;
          	_putchar('\n');
-															}
+	}
 }
