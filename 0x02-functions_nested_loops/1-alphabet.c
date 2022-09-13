1d92dd99f16effc5a6169bdb01d58d844967f908
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-13 12:08:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet of letters
+ *
+ * return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= ''z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
