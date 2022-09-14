8bd95a9675e1a20b436bc89383da53ec9ad6218b
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-13 17:02:01 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - check if character is a alphabet .
+ * @c: type int character
+ * REturn: 1 if letter, lowercase or uppercase, and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+
+	else
+		return(0);
+}
