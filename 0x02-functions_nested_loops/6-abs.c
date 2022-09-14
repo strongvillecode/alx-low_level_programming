128bd9f273ceeb41b88fba42ad8e83cf316fd9ae
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-13 18:07:03 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,24 +1,10 @@
 #include "main.h"
 /**
- * print_sign - a function that prints the sign of a number
+ * _abs - a function that computes the absolute value of an integer
  * @n: number input
- * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0 
+ * Return: absolute value of ab
  */
-int print_sign(int n)
+int _abs(int ab)
 {
-	if (n > 0)
-	{
-		_putchar ('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar ('0');
-		return (0);
-	}
-	else
-	{
-		_putchar ('-');
-		return (-1);
-	}
+	return (ab * ((ab > 0) - (ab < 0)));
 }
