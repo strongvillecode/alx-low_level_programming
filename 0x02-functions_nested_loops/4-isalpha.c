46807f27afda4b60c9674ce769cf4c3f04fe3a5e
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-13 17:24:46 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - check if character is a alphabet.
  * @c: type int character
- * REturn: 1 if letter, lowercase or uppercase, and 0 otherwise
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
