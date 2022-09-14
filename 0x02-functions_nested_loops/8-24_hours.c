077f652001c669acd95fb06612cd7d5438b25b37
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-13 18:30:46 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((m % 10) + '0');
+			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
