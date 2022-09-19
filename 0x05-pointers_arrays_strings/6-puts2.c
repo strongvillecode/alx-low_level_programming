ad25e5848b57a689ee052e677e4c72d3901265a8
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-09-19 13:19:50 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
- * @s: string to be reversed
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
 
-void rev_string(char *s)
+void puts2(char *str)
 {
-	char tmp;
-	int i, len, len1;
+	int len, i;
 
 	len = 0;
-	len1 = 0;
 
-	while (s[len] != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
 	}
 
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
+	for (i = 0; i < len; i += 2)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		_putchar(str[i]);
 	}
+
+	_putchar('\n');
 }
