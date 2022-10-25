db36ca6be9be124b9061dd439792db48f636a7e8
strongvillecode <107783160+strongvillecode@users.noreply.github.com>
2022-10-25 23:25:30 +0100

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,6 @@
  * get_nodeint_at_index - finds a given node in a linked list
  * @head : pointer to the head of the linked list
  * @index: index of the node to find (indices starting at 0)
-
  * Return: pointer to the desired node,
  *         or NULL - if the node does not exist
  */
